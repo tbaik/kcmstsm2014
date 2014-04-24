dbc19a053ab19b8894a3b4b337a6485c1b53a4bf
shyunz <baiktony@yahoo.com>
2014-04-24 01:42:47 -0700

    caching change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ KcmOnline::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
@@ -12,7 +12,7 @@ KcmOnline::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
